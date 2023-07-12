b857c32751f94452f84cc3f094221d9bdecf63bd
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-07-12 17:11:58 +0530

    feat: change primary to secondary

--- src/services/contact.js
+++ src/services/contact.js
@@ -8,7 +8,7 @@ const db = require('../../database/models/index');
 
 //contact response
 const identifyContact = async (contact) => {
-    const linkedPrimaryContacts = await db.Contact.findAll({
+    const allLinkedContacts = await db.Contact.findAll({
         where:
             Sequelize.or({
                 email: contact.email
@@ -17,7 +17,7 @@ const identifyContact = async (contact) => {
             },)
     });
     //if no linked contacts
-    if (linkedPrimaryContacts === null || linkedPrimaryContacts.length === 0) {
+    if (allLinkedContacts === null || allLinkedContacts.length === 0) {
         const newContact = await db.Contact.create({
             email: contact.email,
             phoneNumber: contact.phoneNumber.toString(),
@@ -36,27 +36,47 @@ const identifyContact = async (contact) => {
     else {
         //if searching through just email or phone
         if (contact.email === null || contact.phoneNumber === null) {
-            const primaryContact = linkedPrimaryContacts.filter((contact) => contact.linkPrecedence === 'primary');
+            const primaryContact = allLinkedContacts.filter((contact) => contact.linkPrecedence === 'primary');
             const contactResponse = {
                 contact: {
-                    primaryContactId: primaryContact.id,
-                    emails: linkedPrimaryContacts.map((contact) => contact.email),
-                    phoneNumbers: linkedPrimaryContacts.map((contact) => contact.phoneNumber),
-                    secondaryContactIds: linkedPrimaryContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
+                    primaryContactId: primaryContact[0].id,
+                    emails: allLinkedContacts.map((contact) => contact.email),
+                    phoneNumbers: allLinkedContacts.map((contact) => contact.phoneNumber),
+                    secondaryContactIds: allLinkedContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
                 }
             };
             return contactResponse;
         }
+
         //creating secondary contact when only one primary contact exists
-        if (linkedPrimaryContacts.length === 1 && linkedPrimaryContacts[0].linkPrecedence === 'primary') {
+        if (allLinkedContacts.length === 1 && allLinkedContacts[0].linkPrecedence === 'primary') {
             //if email and phone number are not same
-            if (contact.email !== linkedPrimaryContacts[0].email || contact.phoneNumber.toString() !== linkedPrimaryContacts[0].phoneNumber) {
-                const newLinkedContact = await db.Contact.create({
-                    email: contact.email !== linkedPrimaryContacts[0].email ? contact.email : linkedPrimaryContacts[0].email,
-                    phoneNumber: contact.phoneNumber !== linkedPrimaryContacts[0].phoneNumber ? contact.phoneNumber.toString() : linkedPrimaryContacts[0].phoneNumber.toString(),
-                    linkPrecedence: 'secondary',
-                    linkedId: linkedPrimaryContacts[0].id,
-                });
+            if (contact.email !== allLinkedContacts[0].email || contact.phoneNumber.toString() !== allLinkedContacts[0].phoneNumber) {
+                if (contact.email === null) {
+                    await db.Contact.create({
+                        email: allLinkedContacts[0].email,
+                        phoneNumber: contact.phoneNumber.toString(),
+                        linkPrecedence: 'secondary',
+                        linkedId: allLinkedContacts[0].id,
+                    });
+                }
+                else if (contact.phoneNumber === null) {
+                    await db.Contact.create({
+                        email: contact.email,
+                        phoneNumber: allLinkedContacts[0].phoneNumber.toString(),
+                        linkPrecedence: 'secondary',
+                        linkedId: allLinkedContacts[0].id,
+                    });
+                }
+                else {
+                    const newLinkedContact = await db.Contact.create({
+                        email: contact.email !== allLinkedContacts[0].email ? contact.email : allLinkedContacts[0].email,
+                        phoneNumber: contact.phoneNumber !== allLinkedContacts[0].phoneNumber ? contact.phoneNumber.toString() : allLinkedContacts[0].phoneNumber.toString(),
+                        linkPrecedence: 'secondary',
+                        linkedId: allLinkedContacts[0].id,
+                    });
+                }
+
             }
             const linkedContacts = await db.Contact.findAll({
                 where:
@@ -66,41 +86,109 @@ const identifyContact = async (contact) => {
                         phoneNumber: contact.phoneNumber !== null ? contact.phoneNumber.toString() : ""
                     },)
             });
-
+            const emails = linkedContacts.map((contact) => contact.email);
+            const uniqueEmails = emails.filter((email, index) => emails.indexOf(email) === index);
+            const phoneNumbers = linkedContacts.map((contact) => contact.phoneNumber);
+            const uniquePhoneNumbers = phoneNumbers.filter((phoneNumber, index) => phoneNumbers.indexOf(phoneNumber) === index);
             const contactResponse = {
                 contact: {
                     primaryContactId: linkedContacts.filter((contact) => contact.linkPrecedence === 'primary')[0].id,
-                    emails: linkedContacts.map((contact) => contact.email),
-                    phoneNumbers: linkedContacts.filter((item,
-                        index) => linkedContacts.indexOf(item) === index).map((contact) => contact.phoneNumber),
-                    secondaryContactIds: linkedContacts.map((contact) => contact.id),
+                    emails: uniqueEmails,
+                    phoneNumbers: uniquePhoneNumbers,
+                    secondaryContactIds: linkedContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
                 }
             };
             return contactResponse;
         }
-        //updating primary to secondary contact
         else {
+            const primaryContacts = allLinkedContacts.filter((contact) => contact.linkPrecedence === 'primary');
+
+            //creating another secondary when matching contact is also secondary
+            if (primaryContacts.length == 0) {
+                const secondaryContact = allLinkedContacts.filter((contact) => contact.linkPrecedence === 'secondary')[0];
+                const primaryContact = await db.Contact.findOne({
+                    where: {
+                        id: secondaryContact.linkedId,
+                    }
+                });
+                await db.Contact.create({
+                    email: contact.email !== secondaryContact.email ? contact.email : secondaryContact.email,
+                    phoneNumber: contact.phoneNumber !== secondaryContact.phoneNumber ? contact.phoneNumber.toString() : secondaryContact.phoneNumber.toString(),
+                    linkPrecedence: 'secondary',
+                    linkedId: primaryContact.id,
+                });
+                const linkedContacts = await db.Contact.findAll({
+                    where: Sequelize.or({ id: primaryContact.id }, { linkedId: primaryContact.id }),
+                });
+                const emails = linkedContacts.map((contact) => contact.email);
+                const uniqueEmails = emails.filter((email, index) => emails.indexOf(email) === index);
+                const phoneNumbers = linkedContacts.map((contact) => contact.phoneNumber);
+                const uniquePhoneNumbers = phoneNumbers.filter((phoneNumber, index) => phoneNumbers.indexOf(phoneNumber) === index);
+                const contactResponse = {
+                    contact: {
+                        primaryContactId: primaryContact.id,
+                        emails: uniqueEmails,
+                        phoneNumbers: uniquePhoneNumbers,
+                        secondaryContactIds: linkedContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
+                    }
+                };
+                return contactResponse;
+            }
+
+            //creating another secondary contact for primary contact
+
+            if (primaryContacts.length == 1) {
+                await db.Contact.create({
+                    email: contact.email !== primaryContacts[0].email ? contact.email : primaryContacts[0].email,
+                    phoneNumber: contact.phoneNumber !== primaryContacts[0].phoneNumber ? contact.phoneNumber.toString() : primaryContacts[0].phoneNumber.toString(),
+                    linkPrecedence: 'secondary',
+                    linkedId: primaryContacts[0].id,
+                });
+                const linkedContacts = await db.Contact.findAll({
+                    where: Sequelize.or({ id: primaryContacts[0].id }, { linkedId: primaryContacts[0].id }),
+                });
+                const emails = linkedContacts.map((contact) => contact.email);
+                const uniqueEmails = emails.filter((email, index) => emails.indexOf(email) === index);
+                const phoneNumbers = linkedContacts.map((contact) => contact.phoneNumber);
+                const uniquePhoneNumbers = phoneNumbers.filter((phoneNumber, index) => phoneNumbers.indexOf(phoneNumber) === index);
+                const contactResponse = {
+                    contact: {
+                        primaryContactId: primaryContacts[0].id,
+                        emails: uniqueEmails,
+                        phoneNumbers: uniquePhoneNumbers,
+                        secondaryContactIds: linkedContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
+                    }
+                };
+                return contactResponse;
+
+            }
+            const recentPrimaryContactId = primaryContacts.sort((a, b) => a.createdAt - b.createdAt)[1].id;
+            const oldestPrimaryContactId = primaryContacts.sort((a, b) => a.createdAt - b.createdAt)[0].id;
 
             await db.Contact.update({
                 linkPrecedence: 'secondary',
+                linkedId: oldestPrimaryContactId,
             }, {
                 where: {
-                    id: linkedPrimaryContacts.sort((a, b) => a.createdAt - b.createdAt)[1].id,
+                    id: recentPrimaryContactId,
                 }
             });
             const linkedContacts = await db.Contact.findAll({
                 where: {
-                    linkedId: isLinkedContact.id,
-                    linkPrecedence: 'secondary',
+                    linkedId: oldestPrimaryContactId,
                 }
             });
 
+            const emails = linkedContacts.map((contact) => contact.email);
+            const uniqueEmails = emails.filter((email, index) => emails.indexOf(email) === index);
+            const phoneNumbers = linkedContacts.map((contact) => contact.phoneNumber);
+            const uniquePhoneNumbers = phoneNumbers.filter((phoneNumber, index) => phoneNumbers.indexOf(phoneNumber) === index);
             const contactResponse = {
                 contact: {
-                    primaryContactId: isLinkedContact.id,
-                    emails: [isLinkedContact.email].concat(linkedContacts.map((contact) => contact.email)),
-                    phoneNumbers: linkedContacts.map((contact) => contact.phoneNumber),
-                    secondaryContactIds: linkedContacts.map((contact) => contact.id),
+                    primaryContactId: oldestPrimaryContactId,
+                    emails: uniqueEmails,
+                    phoneNumbers: uniquePhoneNumbers,
+                    secondaryContactIds: linkedContacts.filter((contact) => contact.linkPrecedence === 'secondary').map((contact) => contact.id),
                 }
             };
             return contactResponse;
